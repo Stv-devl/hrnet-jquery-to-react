8bab235938063b11d9e67225ee90aab22b5dd2d6
Stv-devl <lansadstevan@gmail.com>
2024-03-25 11:40:51 +0700

    rooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Banner from "./components/banner/Banner";
+import Home from "./pages/home/Home";
+import Employee from "./pages/employee/Employee";
+import Add from "./pages/add/Add";
+import Error from "./components/erreur/Error";
+import Footer from "./components/footer/Footer";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Banner />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/employee" element={<Employee />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
