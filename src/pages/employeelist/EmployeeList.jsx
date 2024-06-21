009d4a810ee588832ed9dde39ceb98e5ce6437b3
Stv-devl <lansadstevan@gmail.com>
2024-06-21 16:47:37 +0700

    set css props

--- src/pages/employeelist/EmployeeList.jsx
+++ src/pages/employeelist/EmployeeList.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import { ApiContext } from "../../context/ManageApi";
 import { datas } from "../../data/datas";
 import Loading from "../../components/loading/Loading";
-import Table from "../../components/table/Table";
-/*import Table from "react-simple-table-component-v1";*/
+/*import Table from "../../components/table/Table";*/
+import Table from "react-simple-table-component-v1";
 
 /**
  * EmployeeList component
