8c2c16bd4f810e9e7db03b8b07656057a405e796
Stv-devl <lansadstevan@gmail.com>
2024-06-25 16:42:05 +0700

    dp test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import CreateEmployee from "./pages/createemployee/CreateEmployee";
 import Error from "./components/error/Error";
 import Footer from "./components/footer/Footer";
 
+/*  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "server": "json-server --watch src/data/workers.json --port 3004",
+    "deploy": "npm run build && node server.js"
+  },*/
+
 const App = () => {
   return (
     <BrowserRouter>
