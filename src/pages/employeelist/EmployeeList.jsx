f8ff471f340d291a5b00dcd2bca153374375e253
Stv-devl <lansadstevan@gmail.com>
2024-06-26 11:33:53 +0700

    deploy

--- src/pages/employeelist/EmployeeList.jsx
+++ src/pages/employeelist/EmployeeList.jsx
@@ -3,8 +3,8 @@ import { ApiContext } from "../../context/ManageApi";
 import { datas } from "../../data/datas";
 import Loading from "../../components/loading/Loading";
 import Error from "../../components/error/Error";
-import Table from "../../components/table/Table";
-/*import Table from "react-simple-table-component-v1";*/
+/*import Table from "../../components/table/Table";*/
+import Table from "react-simple-table-component-v1";
 
 /**
  * EmployeeList component
